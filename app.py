504b5ed97965e4e7ddb2c21f0ad2003ac813e327
Taaanet <93224771+Taaanet@users.noreply.github.com>
2025-09-13 14:06:48 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -614,3 +614,8 @@ if __name__ == '__main__':
     
     # Run the application
     app.run(debug=True, host='0.0.0.0', port=available_port, threaded=True)
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
